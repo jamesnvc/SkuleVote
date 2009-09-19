3df4924f15da0afb10d54ae052fc5e9d692e998b
James Cash <james@user131-137.wireless.utoronto.ca>
2009-09-19 17:53:04 -0400

    Don't consider spoiled ballots in candidate consideration after determining number, also factoring out common tie-breaking code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,9 +18,19 @@ module ApplicationHelper
     if spoiled > total/2.0
       return [:spoiled , spoiled] # How do we want to indicate this?
     end
+
+    # For most of the next calculations, we only want actual candidates, i.e. not spoiled ballots
+    real_candidates = candidates.reject {|k,v| k == 0 }
     
-    leader_id, leader_votes = candidates.max { |a,b| a[1] <=> b[1] }
-    loser_id, loser_votes = candidates.min { |a,b| a[1] <=> b[1] }
+    # Find the candidates with the most and least votes
+    leader_id, leader_votes = real_candidates.max { |a,b| a[1] <=> b[1] }
+    loser_id, loser_votes = real_candidates.min { |a,b| a[1] <=> b[1] }
+    
+    resolve_tie = lambda do # have to use a lambda to capture variables in the outer scope
+      # Currently just return all the tied candidates
+      tied = real_candidates.select { |c_id,votes| votes == leader_votes }
+      return [tied.map { |c_id,votes| c_id }, spoiled]
+    end
     
     if loser_votes == leader_votes
       # There must be either a tie or a degenerate election
@@ -28,17 +38,16 @@ module ApplicationHelper
         return [leader_id, spoiled]
       else
         # TODO: add tie-breaking code here
-        return [candidates.select { |c_id,votes|
-          votes == leader_votes and c_id != 0 }.map { |c_id,votes| c_id }, spoiled]
+        resolve_tie.call
       end
     end
     
     if leader_votes > total/2.0
       # max only returns one candidate, so check if two have the same amount of votes
-      if candidates.select { |c_id,votes| votes == leader_votes }.size > 1
+      if real_candidates.values.select { |votes| votes == leader_votes }.size > 1
         # Tie; return the tied candidates
         # TODO: Add tie-breaking code here
-        return [candidates.select { |c_id,votes| votes == leader_votes }.map { |c_id,votes| c_id }, spoiled]
+        resolve_tie.call
       else
         return [leader_id, spoiled]
       end
